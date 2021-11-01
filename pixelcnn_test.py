954d3e1820eea8347e3d9e9e8065001cb9eecd7e
phillipsi <>
2021-11-01 15:51:26 -0400

    changes

---
+++ pixelcnn_test.py
@@ -0,0 +1,113 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tqdm import tqdm
+
+# Model / data parameters
+num_classes = 10
+input_shape = (28, 28, 1)
+n_residual_blocks = 5
+# The data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x = x_train.reshape(x_train.shape[0], -1).astype(np.float32)[y_train ==5]
+x_valid = x_test.reshape(x_test.shape[0], -1).astype(np.float32)[y_test ==5]
+y = y_train[y_train==5]
+y_valid = y_test[y_test==5]
+# Concatenate all of the images together
+data = np.concatenate((x, y), axis=0)
+# Round all pixel values less than 33% of the max 256 value to 0
+# anything above this value gets rounded up to 1 so that all values are either
+# 0 or 1
+data = np.where(data < (0.33 * 256), 0, 1)
+data = data.astype(np.float32)
+
+
+# The first layer is the PixelCNN layer. This layer simply
+# builds on the 2D convolutional layer, but includes masking.
+class PixelConvLayer(layers.Layer):
+    def __init__(self, mask_type, **kwargs):
+        super(PixelConvLayer, self).__init__()
+        self.mask_type = mask_type
+        self.conv = layers.Conv2D(**kwargs)
+
+    def build(self, input_shape):
+        # Build the conv2d layer to initialize kernel variables
+        self.conv.build(input_shape)
+        # Use the initialized kernel to create the mask
+        kernel_shape = self.conv.kernel.get_shape()
+        self.mask = np.zeros(shape=kernel_shape)
+        self.mask[: kernel_shape[0] // 2, ...] = 1.0
+        self.mask[kernel_shape[0] // 2, : kernel_shape[1] // 2, ...] = 1.0
+        if self.mask_type == "B":
+            self.mask[kernel_shape[0] // 2, kernel_shape[1] // 2, ...] = 1.0
+
+    def call(self, inputs):
+        self.conv.kernel.assign(self.conv.kernel * self.mask)
+        return self.conv(inputs)
+
+
+# Next, we build our residual block layer.
+# This is just a normal residual block, but based on the PixelConvLayer.
+class ResidualBlock(keras.layers.Layer):
+    def __init__(self, filters, **kwargs):
+        super(ResidualBlock, self).__init__(**kwargs)
+        self.conv1 = keras.layers.Conv2D(
+            filters=filters, kernel_size=1, activation="relu"
+        )
+        self.pixel_conv = PixelConvLayer(
+            mask_type="B",
+            filters=filters // 2,
+            kernel_size=3,
+            activation="relu",
+            padding="same",
+        )
+        self.conv2 = keras.layers.Conv2D(
+            filters=filters, kernel_size=1, activation="relu"
+        )
+
+    def call(self, inputs):
+        x = self.conv1(inputs)
+        x = self.pixel_conv(x)
+        x = self.conv2(x)
+        return keras.layers.add([inputs, x])
+
+inputs = keras.Input(shape=input_shape)
+x = PixelConvLayer(
+    mask_type="A", filters=128, kernel_size=7, activation="relu", padding="same"
+)(inputs)
+
+for _ in range(n_residual_blocks):
+    x = ResidualBlock(filters=128)(x)
+
+for _ in range(2):
+    x = PixelConvLayer(
+        mask_type="B",
+        filters=128,
+        kernel_size=1,
+        strides=1,
+        activation="relu",
+        padding="valid",
+    )(x)
+
+out = keras.layers.Conv2D(
+    filters=1, kernel_size=1, strides=1, activation="sigmoid", padding="valid"
+)(x)
+
+pixel_cnn = keras.Model(inputs, out)
+adam = keras.optimizers.Adam(learning_rate=0.0005)
+pixel_cnn.compile(optimizer=adam, loss="binary_crossentropy")
+
+pixel_cnn.summary()
+
+checkpoint_filepath = './checkpoints_pixelcnn/checkpoint'
+quantile_regressor.load_weights(checkpoint_filepath)
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+            filepath=checkpoint_filepath,
+                save_weights_only=True,
+                    monitor='loss',
+                        mode='max',
+                            save_best_only=False)
+pixel_cnn.fit(
+    x=data, y=data, batch_size=128, epochs=50, validation_split=0.1, verbose=2, callbacks=[model_checkpoint_callback]
+)
